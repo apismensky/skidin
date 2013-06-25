c5ef0cd671b4dd9b1f874f84192e59e2135cda4e
apismensky <alexey.pismenskiy@octanner.com>
2013-06-25 14:02:46 -0600

    foo bar rules

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Untitled::Application.config.secret_token = '16dfbc383de626fed0e814f657d1fe8853affc9211b5448c24cfc004b84ba09be3328e189b33925f463b7f3ad68848b922419bda21c3913d63b5e7a8e0704d17'
