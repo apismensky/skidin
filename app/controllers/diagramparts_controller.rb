520671518763cab2881f99d21aa6fb1eeb2dfc6d
apismensky <alexey.pismenskiy@octanner.com>
2013-09-25 22:44:58 -0600

    fix routes

--- app/controllers/diagramparts_controller.rb
+++ app/controllers/diagramparts_controller.rb
@@ -1,13 +1,13 @@
 class DiagrampartsController < ApplicationController
 
 
-#  def index
-#    @products = Diagrampart.where("sysid in (229,495,516,497, 519, 583, 508, 506)").limit 16
-#  end
+  def index
+    @products = Diagrampart.where("sysid in (37822, 38002, 38084, 38605, 38872, 39122, 39650, 39956, 40286, 40360)").limit 16
+  end
 
-#  def show
-#    @product = Diagrampart.find_by_sysid(params[:id])
-#  end
+  def show
+    @product = Diagrampart.find_by_sysid(params[:id])
+  end
 
   def find_by_partno
     @parts = Diagrampart.where("partno='#{params['partno']}'")
@@ -24,10 +24,10 @@ class DiagrampartsController < ApplicationController
     render json: @parts
   end
 
-#  def order
-#    cart = Cart.first
-#    @success = true
-#    cart.destroy if cart
-#  end
+  def order
+    cart = Cart.first
+    @success = true
+    cart.destroy if cart
+  end
 
 end
