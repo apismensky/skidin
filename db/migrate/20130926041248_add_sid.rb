3e5bd3b923b5550c74c510b9312615abeeb5beac
apismensky <alexey.pismenskiy@octanner.com>
2013-09-25 22:18:34 -0600

    delete migration

--- db/migrate/20130926041248_add_sid.rb
+++
@@ -1,7 +0,0 @@
-class AddSid < ActiveRecord::Migration
-  def up
-    add_column :diagramparts, :sysid, :integer
-    execute "CREATE SEQUENCE diagrampart_sysid_seq"
-    execute "UPDATE diagramparts SET sysid=nextval('diagrampart_sysid_seq');"
-  end
-end
